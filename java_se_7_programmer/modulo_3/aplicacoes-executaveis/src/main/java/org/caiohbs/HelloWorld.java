f5b2b6a676341fbb67b85a6aa4b0bc38c4c5f318
Caio Bianchi <caiohbs23@gmail.com>
2024-04-09 10:53:54 -0300

    adicionado novo projeto

---
+++ java_se_7_programmer/modulo_3/aplicacoes-executaveis/src/main/java/org/caiohbs/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.caiohbs;
+
+public class HelloWorld {
+
+    // Para que uma classe Java seja executável, ela necessariamente precisa ter
+    // um método "main":
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
